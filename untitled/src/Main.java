5f19be888328fb93bf49e392ad462d942365fcf1
Răzvan Sucigan <115634927+SuciganRazvan@users.noreply.github.com>
2022-12-12 19:51:33 +0200

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    private static final String[] COLUMN_NAMES =  {"First Name",
+            "Last Name",
+            "Sport",
+            "# of Years",
+            "Vegetarian"};
+    private static final Object[][] DATA = {
+            {"Kathy", "Smith",
+                    "Snowboarding", 5, false},
+            {"John", "Doe",
+                    "Rowing", 3, true},
+            {"Sue", "Black",
+                    "Knitting", 2, false},
+            {"Jane", "White",
+                    "Speed reading", 20, true},
+            {"Joe", "Brown",
+                    "Pool", 10, false}
+    };
+
+    public static void main(String[] args) {
+
+
+        JFrame container = new JFrame("VEGETARIENI");
+        JTable table = new JTable(DATA, COLUMN_NAMES);
+        table.setCellSelectionEnabled(true);
+        Font font = new Font("Verdana", Font.PLAIN, 12);
+
+        ListSelectionListenerImpl listSelectionListener = new ListSelectionListenerImpl(table);
+
+        ListSelectionModel select = table.getSelectionModel();
+        select.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        select.addListSelectionListener(listSelectionListener);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        container.add(scrollPane);
+        container.setLayout(new BorderLayout());
+        container.add(table.getTableHeader(), BorderLayout.PAGE_START);
+        container.add(table, BorderLayout.CENTER);
+        container.setVisible(true);
+        container.setSize(300, 400);
+
+        table.setBackground(Color.BLACK);
+        table.getTableHeader().setBackground(Color.BLACK);
+        table.setFont(font);
+        table.setForeground(Color.white);
+        JScrollPane sp = new JScrollPane(table);
+        container.add(sp);
+        container.setSize(300, 200);
+        container.setVisible(true);
+
+    }
+}
